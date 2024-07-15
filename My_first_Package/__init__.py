1cebe1d3935398242b69fc949742b9fcc6f5d7cb
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-14 21:19:32 -0700

    Update __init__.py

--- My_first_Package/__init__.py
+++ My_first_Package/__init__.py
@@ -1,2 +1,43 @@
-def calculate_regions(int: lines):
+from math import factorial as fact
+
+def permutations(int: n, int: k) -> int:
+    return fact(n) // fact(n - k)
+
+def combinations(int: n, int: k) -> int:
+    return fact(n) // (fact(k) * fact(n - k))
+
+def calculate_regions(int: lines) -> int:
     return (lines * (lines + 1)) // 2 + 1
+
+def fibonacci(int: num) -> int:
+    if num <= 0:
+        return []
+    elif num == 1:
+        return [0]
+    elif num == 2:
+        return [0, 1]
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+
+def prime_factors(int: num) -> int:
+    factors = []
+    divisor = 2
+    while num > 1:
+        while num % divisor == 0:
+            factors.append(divisor)
+            num //= divisor
+        divisor += 1
+    return factors
+
+
+def gcd(int: num1, int: num2) -> int:
+    while num2:
+        num1, num2 = num2, num1 % num2
+    return a
+
+
+def lcm(int: num1, int: num2) -> int:
+    return abs(num1 * num2) // gcd(num1, num2)
